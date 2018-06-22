8b7567ec02e0fc8070b6e1a64e7a065c6f557d34
saw-mill <saumil.sachdeva@gmail.com>
2018-06-22 13:52:30 +0200

    Converting client to an array

--- src/mpMultiInterfaceTopo.py
+++ src/mpMultiInterfaceTopo.py
@@ -7,7 +7,7 @@ class MpMultiInterfaceTopo(MpTopo):
 		count=int(MpTopo.clientCount)
 		self.client =[]
 		for i in range (1,count):
-			self.client[i]=self.addHost("Client"+str(i))
+			self.client.append(i)=self.addHost("Client"+str(i))
 		# self.client1 = self.addHost(MpTopo.clientName1)
 		# self.client2 = self.addHost(MpTopo.clientName2)
 		self.server = self.addHost(MpTopo.serverName)
